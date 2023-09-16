407b8b5a8ffb8c0d1a52a4ca52218cba932633b2
Siva Pulagam <vallururohit@gmail.com>
2023-09-16 19:44:06 +0530

    Fixed Computer canvas in Android

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,30 +1,25 @@
-import { motion } from 'framer-motion';
 import { styles } from '../styles';
-import { ComputersCanvas } from './canvas';
+import car3 from '../assets/company/car3.png'
 
 const Hero = () => {
   return (
-    <section className='relative w-full h-screen mx-auto'>
-      <div className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5`}>
-        <div className='flex flex-col justify-center items-center mt-5'>
-          <div className='w-5 h-5 rounded-full bg-[#915eff]'></div>
-          <div className='w-1 sm:h-80 h-40 violet-gradient'></div>
-        </div>
-        <div>
-          <h1 className={`${styles.heroHeadText}`}>Hi, I'm<span className='text-[#915eff]'> Rohit </span></h1>
-          <p className={`${styles.heroSubText} mt-2 text-white-100`}> An aspiring web developer with a passion for coding and creating interactive web experiences.</p>
-        </div>
-      </div>
-      <ComputersCanvas/>
-
-      <div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center">
-        <a href='#about'>
-          <div className="w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2">
-            <motion.dev animate={{ y: [0, 24, 0] }} transition={{ duration: 1.5, repeat: Infinity, repeatType: "loop" }} className="w-3 h-3 rounded-full bg-secondary mb-1"></motion.dev>
+    <>
+      <section className='relative w-full mx-auto'>
+        <div className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5`}>
+          <div className='flex flex-col justify-center items-center mt-5'>
+            <div className='w-5 h-5 rounded-full bg-[#915eff]'></div>
+            <div className='w-1 sm:h-80 h-40 violet-gradient'></div>
           </div>
-        </a>
+          <div>
+            <h1 className={`${styles.heroHeadText}`}>Hi, I'm<span className='text-[#915eff]'> Rohit </span></h1>
+            <p className={`${styles.heroSubText} mt-2 text-white-100`}> An aspiring web developer with a passion for coding and creating interactive web experiences.</p>
+          </div>
+        </div>
+      </section>
+      <div>
+        <img src={car3} />
       </div>
-    </section>
+    </>
   )
 }
 
